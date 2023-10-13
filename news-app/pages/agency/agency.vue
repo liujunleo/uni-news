47f00902c83e9f59f06801203055e0ab5c664be2
刘俊 <793898892@qq.com>
2023-10-13 17:20:30 +0800

    init

---
+++ news-app/pages/agency/agency.vue
@@ -0,0 +1,14 @@
+<!-- 网络链接内容展示页（uni-id-pages中用于展示隐私政策协议内容） -->
+<template>
+	<view>
+		<web-view src="https://www.baidu.com"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		onLoad() {
+			uni.setNavigationBarTitle({title: '人民号'});
+		}
+	}
+</script>
